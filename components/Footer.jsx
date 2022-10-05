e5a9dfda1a548e46872418482c7e74326adbdad4
prathibha <prsthibha@gmail.com>
2022-10-05 19:09:18 +0530

    Add Navbar and Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
+  const year = new Date().getFullYear()
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>{year} COOL Headphones | All rights reserved</p>
+      <p className='icons'>
+        <AiOutlineInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
   )
 }
 
