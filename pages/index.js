609b79ecceecac3d72ec5e0fd60fefce44eb8b12
prathibha <prsthibha@gmail.com>
2022-10-05 18:50:13 +0530

    Add FooterBanner component

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ const Home = ({ products, bannerData }) => {
       </div>
 
       <div className='products-container'>
-      {products?.map((product) => <Product key={product._id} product={product} />)}
+        {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
 
 
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   )
 }
