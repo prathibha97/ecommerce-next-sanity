da93a16f9cf2e9bae94861647a829f1c5c7e95e3
prathibha <prsthibha@gmail.com>
2022-10-06 15:27:56 +0530

    Add functionality to Buy now button

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -10,7 +10,12 @@ function ProductDetails({ product, products }) {
 
     const [index, setIndex] = useState(0)
 
-    const { decQty, incQty, qty, onAdd } = useStateContext()
+    const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext()
+
+    const handleBuyNow = () => {
+        onAdd(product, qty)
+        setShowCart(true)
+    }
 
     return (
         <div>
@@ -60,8 +65,8 @@ function ProductDetails({ product, products }) {
                     <div className="buttons">
                         <button type="button" className="add-to-cart"
                             onClick={() => onAdd(product, qty)}>Add to Cart</button>
-                        <button type="button" className="buy-now"
-                            >Buy Now</button>
+                        <button type="button" className="buy-now" onClick={handleBuyNow}
+                        >Buy Now</button>
                     </div>
                 </div>
             </div>
