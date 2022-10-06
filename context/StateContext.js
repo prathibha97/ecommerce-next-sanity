adc113817c5167c541e0eb33293f0c1ec254df18
prathibha <prsthibha@gmail.com>
2022-10-06 15:12:01 +0530

    Import states

--- context/StateContext.js
+++ context/StateContext.js
@@ -100,7 +100,10 @@ export const StateContext = ({ children }) => {
             onAdd,
             setShowCart,
             toggleCartItemQuantity,
-            onRemove
+            onRemove,
+            setCartItems,
+            setTotalPrice,
+            setTotalQuantities,
         }}>
             {children}
         </Context.Provider>
